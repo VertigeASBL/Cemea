4073747fda8185d4f49f1c86334352bbef95ac1e
phenix <trash@phenix-factory.net>
2013-01-03 15:06:43 +0100

    Filtrage des certificat sur le champ ASBL

--- plugins/gestion_cemea/formulaires/creer_certificat.php
+++ plugins/gestion_cemea/formulaires/creer_certificat.php
@@ -1,7 +1,7 @@
 <?php
-function formulaires_creer_certificat_charger_dist() {
+function formulaires_creer_certificat_charger_dist($id_article, $asbl) {
         $contexte = array(
-                '' => '',
+                'asbl' => $asbl
         );
         return $contexte;
 }
