fa2a5e370f596f9bde49f3e4c40044bd0386a5ad
phenix-factory <trash@henix.be>
2013-05-08 15:57:10 +0200

    Rsolution d'un Warning.

--- plugins/gestion_cemea/formulaires/creer_pdf.php
+++ plugins/gestion_cemea/formulaires/creer_pdf.php
@@ -6,7 +6,7 @@ function formulaires_creer_pdf_charger_dist($id_article, $id_auteur, $asbl) {
         return $contexte;
 }
 
-function formulaires_creer_pdf_verifier_dist() {
+function formulaires_creer_pdf_verifier_dist($id_article, $id_auteur, $asbl) {
         $erreurs = array();
         if (!_request('creer_pdf')) {
                 $erreurs['message_erreur'] = 'Vous devez choisir un document PDF';
@@ -15,7 +15,7 @@ function formulaires_creer_pdf_verifier_dist() {
         return $erreurs;
 }
 
-function formulaires_creer_pdf_traiter_dist($id_article, $id_auteur) {
+function formulaires_creer_pdf_traiter_dist($id_article, $id_auteur, $asbl) {
  		
  		$data = _request('creer_pdf');
 		
