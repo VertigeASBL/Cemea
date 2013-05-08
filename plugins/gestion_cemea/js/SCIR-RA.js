df32ce400e05ffc450e025926a2cb65c94a61fa6
phenix-factory <trash@henix.be>
2013-05-08 17:19:47 +0200

    On affecte le prototype àtout les champs RA-SCIR.

--- plugins/gestion_cemea/js/SCIR-RA.js
+++ plugins/gestion_cemea/js/SCIR-RA.js
@@ -1,17 +1,31 @@
 jQuery(document).ready(function($) {
     
-    $("#titre_ra").blur(function () {
-        // On récupère la valeur du champ.
-        var value_ra = $(this).val();
-        
-        // Si la valeur n'est pas vide.
-        if (value_ra) {
-            // On test la valeur correspondante dans les champs SCIR.
-            var value_scir = $("#titre_scir").val();
+    // Tableau des champs sans le suffixe
+    var champs = [
+                    "#date_",
+                    "#titre_",
+                    "#nb_formateurs_",
+                    "#nb_part_",
+                    "#par_unite_",
+                    "#type_unite_",
+                    "#nb_unite_"
+                    ];
+    
+    // On boucle sur tout les champs
+    $.each(champs, function (index, value) {
+        // On live le blur et on pointe sur le champs + le sufixe RA
+        $(value+"ra").live("blur", function () {
+            // On récupère la valeur du champ.
+            var value_ra = $(this).val();
+            
+            // Si la valeur n'est pas vide.
+            if (value_ra) {
+                // On test la valeur correspondante dans les champs SCIR.
+                var value_scir = $(value+"scir").val();
 
-            // Si la valeur SCIR est vide on prend celle en RA.
-            if (!value_scir) $("#titre_scir").val(value_ra);
-        }
-
-    })
+                // Si la valeur SCIR est vide on prend celle en RA.
+                if (!value_scir) $(value+"scir").val(value_ra);
+            }
+        });
+    });
 });
