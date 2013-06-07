cae490fff880f339129b452cfebb62822391c4a6
phenix-factory <trash@henix.be>
2013-06-07 09:54:19 +0200

    Tri du fichier de langue.

--- plugins/gestion_cemea/lang/gestion_fr.php
+++ plugins/gestion_cemea/lang/gestion_fr.php
@@ -2,108 +2,78 @@
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-	// A
-	'auteur_poubelle' => 'Cet personne à été supprimée.',
-	'age' => 'Age',
-	'adresse' => 'Adresse',
-	'activite' => 'Activité',
-	'action' => 'Option',
-	'a_payer' => 'A payer',
-	'a_rembourser' => 'A rembourser',
-	'acompte' => 'Acompte',
-	'ajouter_inscrire' => 'Ajouter une personne',
-	'activite_proche' => 'Activité proche',
-    'no_no_mail' => 'Aucune personne sans adresse mail',
-
-	// C
-	'confirmation_mail' => 'Un email de confirmation à été envoyer à l\'adresse: ',
-	'changement_statut' => 'Le statut de cette personne à été changé.',
-	'confirmer_inscription' => 'Cette personne à été inscrite',
-	'confirmer' => 'Confirmer ?',
-	'copie' => 'Copie ?',
-	'est_copie' => 'Cet article est une copie de',
-
-	// D
-	'derniere_inscription' => 'Dernière Inscription',
-	'date_validation' => 'Date de validation',
-	'date_naissance' => 'Date de naissance',
-	'date_debut' => 'Date de début',
+    'a_payer' => 'A payer',
+    'a_rembourser' => 'A rembourser',
+    'acompte' => 'Acompte',
+    'action' => 'Option',
+    'activite' => 'Activité',
+    'activite_proche' => 'Activité proche',
+    'adresse' => 'Adresse',
+    'age' => 'Age',
+    'ajouter_inscrire' => 'Ajouter une personne',
+    'auteur_poubelle' => 'Cet personne à été supprimée.',
+    'changement_statut' => 'Le statut de cette personne à été changé.',
+    'confirmation_mail' => 'Un email de confirmation à été envoyer à l\'adresse: ',
+    'confirmer' => 'Confirmer ?',
+    'confirmer_inscription' => 'Cette personne à été inscrite',
+    'copie' => 'Copie ?',
+    'date_debut' => 'Date de début',
+    'date_naissance' => 'Date de naissance',
+    'date_validation' => 'Date de validation',
+    'deja_inscrit' => 'Cette personne est déjà inscrite à cette activité.',
+    'demandeur_emploi' => 'Demandeur d\'emploi/Etudiant',
+    'dernier_annule' => 'Dernière annulation',
+    'dernier_payement' => 'Dernier payement validé',
+    'derniere_inscription' => 'Dernière Inscription',
+    'echeance' => 'Echéance de payement',
+    'ecrire_nouvelle_action' => 'Ajouter une action',
+    'editer_inscrit' => 'Editer un inscrit',
+    'email' => 'Email',
+    'envoyer_pdf' => 'Envoyer le PDF',
+    'envoyer_pdf_confirmer' => 'Le PDF à été envoyé.',
+    'est_copie' => 'Cet article est une copie de',
+    'fax' => 'Fax',
+    'gestion_activite' => 'Gestion des actions',
+    'gestion_inscription' => 'Gestion des inscriptions',
+    'gestion_inscrit' => 'Gestion des inscrits',
+    'gestion_payement' => 'Gestion des payements',
+    'gsm' => 'GSM',
+    'inscription' => 'Inscription',
+    'jour' => 'Jour restant',
+    'limit' => 'Limite',
+    'liste_participant' => 'Liste des participants',
+    'mass_mail' => 'Mail massif',
+    'mettre_auteur_poubelle' => 'Supprimer cette personne',
+    'modifier_inscrit' => 'Modifier',
+    'nom' => 'Nom',
+    'nom_activite' => 'Nom de l\'activité',
+    'nouveau_inscrit' => 'Ajouter une personne',
+    'option' => 'Option de tri',
+    'paye' => 'Payé',
+    'personne' => 'Personne',
+    'prenom' => 'Prénom',
+    'prix_aide' => 'Uniquement le montant, pas de caractère non numérique.',
+    'recherche' => 'Recherche',
+    'rechercher' => 'Rechercher',
+    'recreer_fichier' => 'Ce fichier existe déjà, êtes vous sur de vouloir le recréer ?',
+    'reference' => 'Référence',
+    'remboursement' => 'Remboursement',
+    'renvoyer_existant' => 'Renvoyer le fichier existant'
+    'retour' => 'Retour',
+    'retour_action' => 'Retour',
+    'solde' => 'Solde',
+    'statut' => 'Statut',
+    'statut_payement' => 'Statut payement',
+    'statut_payement_alert' => 'Le statut du payement de cette personne à été changé.',
+    'tableau_de_bord' => 'Tableau de bord',
+    'telecharger_existant' => 'Télécharger le fichier existant',
+    'telecharger_pdf' => 'Télécharger le PDF',
+    'telephone' => 'Téléphone',
     'date_suivi' => 'Date de suivi',
-	'dernier_payement' => 'Dernier payement validé',
-	'deja_inscrit' => 'Cette personne est déjà inscrite à cette activité.',
-	'dernier_annule' => 'Dernière annulation',
-	'demandeur_emploi' => 'Demandeur d\'emploi/Etudiant',
-	
-	// E
-	'ecrire_nouvelle_action' => 'Ajouter une action',
-	'echeance' => 'Echéance de payement',
-	'envoyer_pdf' => 'Envoyer le PDF',
-	'editer_inscrit' => 'Editer un inscrit',
-	'email' => 'Email',
-	'envoyer_pdf_confirmer' => 'Le PDF à été envoyé.',
     'extrait_compte' => 'extrait de compte',
-	// F
-	'fax' => 'Fax',
-
-	// G
-	'gestion_activite' => 'Gestion des actions',
-	'gestion_inscription' => 'Gestion des inscriptions',
-	'gestion_inscrit' => 'Gestion des inscrits',
-	'gestion_payement' => 'Gestion des payements',
-	'gsm' => 'GSM',
-
-	// I
-	'inscription' => 'Inscription',
-
-	// J
-	'jour' => 'Jour restant',
-
-	// L
-	'limit' => 'Limite',
-	'liste_participant' => 'Liste des participants',
-    'liste_payement' => 'Liste des payements',
     'liste_actions_participant' => 'Liste des actions par participants.',
-
-	// N
-	'nom' => 'Nom',
-	'nom_activite' => 'Nom de l\'activité',
-	'nouveau_inscrit' => 'Ajouter une personne',
-
-	// M
-	'modifier_inscrit' => 'Modifier',
-	'mettre_auteur_poubelle' => 'Supprimer cette personne',
-	'mass_mail' => 'Mail massif',
-
-	// O
-	'option' => 'Option de tri',
-	
-	// P
-	'prix_aide' => 'Uniquement le montant, pas de caractère non numérique.',
-	'prenom' => 'Prénom',
-	'personne' => 'Personne',
-	'paye' => 'Payé',
-
-	// S
-	'statut_payement_alert' => 'Le statut du payement de cette personne à été changé.',
-	'statut_payement' => 'Statut payement',
-	'statut' => 'Statut',
-	'solde' => 'Solde',
-	
-	// T
-	'telecharger_pdf' => 'Télécharger le PDF',
-	'tableau_de_bord' => 'Tableau de bord',
-	'telephone' => 'Téléphone',
-	'telecharger_existant' => 'Télécharger le fichier existant',
-
-	// R
-	'remboursement' => 'Remboursement',
-	'rechercher' => 'Rechercher',
-	'recherche' => 'Recherche',
-	'retour' => 'Retour',
-	'retour_action' => 'Retour',
-	'recreer_fichier' => 'Ce fichier existe déjà, êtes vous sur de vouloir le recréer ?',
-	'reference' => 'Référence',
-	'renvoyer_existant' => 'Renvoyer le fichier existant'
+    'liste_payement' => 'Liste des payements',
+    'no_no_mail' => 'Aucune personne sans adresse mail',
 );
 
 ?>
