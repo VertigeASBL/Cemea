fafc7e599d81110e8e4d7133a55ad02880d2bd5c
phenix-factory <trash@henix.be>
2013-10-11 10:16:13 +0200

    statut et pas status

--- plugins/gestion_cemea/exec/gestion_activite_exec.php
+++ plugins/gestion_cemea/exec/gestion_activite_exec.php
@@ -14,7 +14,7 @@ function exec_gestion_activite_exec() {
     // On commence le cadre principal
     echo debut_grand_cadre(true);
 
-    if (session_get('status') == '0minirezo') {
+    if (session_get('statut') == '0minirezo') {
         echo icone_inline(_T('gestion:ecrire_nouvelle_action'), generer_url_ecrire("editer_activite_exec","id_rubrique=43&new=oui"), "article-24.gif","creer.gif", 'right');
     }
     
