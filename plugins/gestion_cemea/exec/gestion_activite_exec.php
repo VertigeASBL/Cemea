11907f6c353a70ad656afdcada43aab0958bd8fd
phenix-factory <trash@henix.be>
2013-10-11 09:51:00 +0200

    Protection de la page de gestion des actions.

--- plugins/gestion_cemea/exec/gestion_activite_exec.php
+++ plugins/gestion_cemea/exec/gestion_activite_exec.php
@@ -14,7 +14,9 @@ function exec_gestion_activite_exec() {
     // On commence le cadre principal
     echo debut_grand_cadre(true);
 
-    echo icone_inline(_T('gestion:ecrire_nouvelle_action'), generer_url_ecrire("editer_activite_exec","id_rubrique=43&new=oui"), "article-24.gif","creer.gif", 'right');
+    if (session_get('status') == '0minirezo') {
+        echo icone_inline(_T('gestion:ecrire_nouvelle_action'), generer_url_ecrire("editer_activite_exec","id_rubrique=43&new=oui"), "article-24.gif","creer.gif", 'right');
+    }
     
     echo '<div class="nettoyeur"></div>';
     
